d7370323c5ce6558d830622b6bcf16e8f5cde64b
Nakzull <andr25f4@zbc.dk>
2021-08-12 09:49:00 +0200

    Add project files.

---
+++ Metoder opgave 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number");
+            double numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number");
+            double numberTwo = Convert.ToInt32(Console.ReadLine());
+            double addedNumbers = AddNumbers(numberOne, numberTwo);
+            double dividedNumbers = DividedNumbers(numberOne, numberTwo);
+            double dividedTotal = DivideTotalTimes(numberOne, numberTwo);
+            double power = NumbersPower(numberOne, numberTwo);
+            Console.WriteLine(dividedTotal);
+        }
+
+        public static double AddNumbers(double numberOne, double numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public static double DividedNumbers(double numberOne, double numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+
+        public static double DivideTotalTimes(double numberOne, double numberTwo)
+        {
+            double divided = numberOne / numberTwo;
+            return Math.Floor(divided);
+        }
+        public static double NumbersPower(double numberOne, double numberTwo)
+        {
+            return Math.Pow(numberOne, numberTwo);
+        }
+    }
+}
